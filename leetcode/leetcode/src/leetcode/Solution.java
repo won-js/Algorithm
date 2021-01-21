0b9217d0d21cb1ab1e52fc57a5f7e3c5c4a05500
won-js <wonjs0116@gmail.com>
2021-01-21 23:02:25 +0900

    20210121

--- leetcode/leetcode/src/leetcode/Solution.java
+++ leetcode/leetcode/src/leetcode/Solution.java
@@ -1,8 +1,26 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
-	public static void main(String [] args) {
-		
-	}
+    public int[] solution(int n, int s) {
+    	List<Integer> list = new ArrayList<>();
+    	if(s<n) return new int[] {-1};
+    	for(int i = n; i>0; i--) {
+    		int a = (int) Math.ceil(s/i);
+    		list.add(a);
+    		s -= a;
+    	}
+    	
+    	list.sort((a,b) -> a-b);
+    	int[] answer = new int[list.size()];
+    	for(int i=0; i<list.size(); i++) {
+    		answer[i] = list.get(i);
+    	}
+    	
+        return answer;
+    }
    
 }

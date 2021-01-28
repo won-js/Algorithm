1ced378fbcede9ead4f26b898d1172fd3f15c6a8
won-js <wonjs0116@gmail.com>
2021-01-28 22:07:07 +0900

    20210128

---
+++ leetcode/leetcode/src/leetcode/ContainDuplicate.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class ContainDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            }
+            else {
+                if ((i - map.get(nums[i])) <= k)
+                    return true;
+                else
+                    map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
